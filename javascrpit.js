9106c357a809d06c414119d5d8e199cbae6ac250
Capi7977 <129705834+Capi7977@users.noreply.github.com>
2024-05-16 00:41:28 -0600

    I have added all the files in on branch

---
+++ javascrpit.js
@@ -0,0 +1,10 @@
+// In the start I created a string where it shows a message of the secret combination
+var message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+// I call out the string so that it can pop out in the the console
+console.log(message)
+// I used unique arithmetic to match the numbers for the seceret vault
+var numb = 5 + 5;
+var numb2 = 80 / 2;
+var numb3 = 40 - 1;
+// I console log the varaibles and added them in one string
+console.log(numb + " - " + numb2 + " - " + numb3)
